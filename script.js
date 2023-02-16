e5eb58f027116f2c26fb60d22979ff4a650f17d7
Jen-nie333 <104903826+Jen-nie333@users.noreply.github.com>
2023-02-15 22:38:05 -0500

    fixed times and dates in js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // London
-function updateTime() {
+function updateLondonTime() {
     let londonElement = document.querySelector("#london");
     if (londonElement) {
     let londonDateElement = londonElement.querySelector(".date");
@@ -16,6 +16,7 @@ setInterval(updateLondonTime, 1000);
 
 
 //New York
+function updateNewYorkTime() {
     let newYorkElement = document.querySelector("#new-york");
     if (newYorkElement) {
     let newYorkDateElement = newYorkElement.querySelector(".date");
@@ -24,9 +25,13 @@ setInterval(updateLondonTime, 1000);
 
     newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
     newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
+updateNewYorkTime();
+setInterval(updateNewYorkTime, 1000);
 
 //Los Angeles
+function updateLosAngelesTime() {
     let losAngelesElement = document.querySelector("#los-angeles");
     if (losAngelesElement) {
       let losAngelesDateElement = losAngelesElement.querySelector(".date");
@@ -38,69 +43,58 @@ setInterval(updateLondonTime, 1000);
         "h:mm:ss [<small>]A[</small>]"
       );
     }
+}
+updateLosAngelesTime();
+setInterval(updateLosAngelesTime, 1000);
+
   
     //Tokyo
+function updateTokyoTime() {
     let tokyoElement = document.querySelector("#tokyo");
     if (tokyoElement) {
       let tokyoDateElement = tokyoElement.querySelector(".date");
       let tokyoTimeElement = tokyoElement.querySelector(".time");
-      let tokyoTime = moment().tz("America/Los_Angeles");
+      let tokyoTime = moment().tz("Asia/Tokyo");
   
-      tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+      tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
       tokyoTimeElement.innerHTML = tokyoTime.format(
         "h:mm:ss [<small>]A[</small>]"
       );
     }
+}
+updateTokyoTime();
+setInterval(updateTokyoTime, 1000);
 
     //Sydney
+function updateSydneyTime() {
     let sydneyElement = document.querySelector("#sydney");
     if (sydneyElement) {
       let sydneyDateElement = sydneyElement.querySelector(".date");
       let sydneyTimeElement = sydneyElement.querySelector(".time");
-      let sydneyTime = moment().tz("America/Los_Angeles");
+      let sydneyTime = moment().tz("Australia/Sydney");
   
       sydneyDateElement.innerHTML = sydneyTime.format("MMMM	Do YYYY");
       sydneyTimeElement.innerHTML = sydneyTime.format(
         "h:mm:ss [<small>]A[</small>]"
       );
     }
+}
+updateSydneyTime();
+setInterval(updateSydneyTime, 1000);
 
 // Paris
+function updateParisTime() {
     let parisElement = document.querySelector("#paris");
     if (parisElement) {
       let parisDateElement = parisElement.querySelector(".date");
       let parisTimeElement = parisElement.querySelector(".time");
       let parisTime = moment().tz("Europe/Paris");
   
-      parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+      parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
       parisTimeElement.innerHTML = parisTime.format(
         "h:mm:ss [<small>]A[</small>]"
       );
     }
-
-function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current") {
-      cityTimeZone = moment.tz.guess();
-    }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
-    <div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-      "A"
-    )}</small></div>
-    </div>
-    `;
-  }
-  
-  updateTime();
-  setInterval(updateTime, 1000);
-  
-  let citiesSelectElement = document.querySelector("#city");
-  citiesSelectElement.addEventListener("change", updateCity);
+}
+updateParisTime();
+setInterval(updateParisTime, 1000);
